49b53b4df9eee07ee1e22ee25c43ab74eed0834d
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-02-08 06:52:49 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,15 @@
-from django.http import HttpResponse
-from django.urls import path, include
 from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
+from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
 
 def home(request):
-    return HttpResponse("Server is running")
+    return HttpResponse("Server is running")  # ✅ simple text
 
 urlpatterns = [
-    path("", home),
+    path("", home),  # root URL
     path("admin/", admin.site.urls),
+    path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path("api/", include("feed.urls")),
 ]
